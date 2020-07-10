74df919eba52819d98c17a9313e1290dd62bf55a
kaanardar <kaan.ardar@betbull.com>
2020-07-10 10:06:30 +0300

    refactor

--- src/main/java/com/demo/airlineservice/service/impl/CardMaskingService.java
+++ src/main/java/com/demo/airlineservice/service/impl/CardMaskingService.java
@@ -9,14 +9,19 @@ import java.util.regex.Pattern;
 @Service
 public class CardMaskingService {
 
-    final Pattern PATTERN = Pattern.compile("^([0-9]{15,16})+$");
+    private final Pattern PATTERN = Pattern.compile("^([0-9]{15,16})+$");
+    private final String SEPARATION = "-";
+    private final String STAR = "*";
+    private final int NUMBER_ZERO = 0;
+    private final int NUMBER_SIX = 6;
+    private final int NUMBER_FOUR = 4;
 
     public String maskCardNumber(String cardNumber) {
         String strippedCreditCard = cardNumber.replaceAll("[ -]+", "");
 
         if (isCardNumberValid(strippedCreditCard)) {
 
-            if (cardNumber.contains("-")) {
+            if (cardNumber.contains(SEPARATION)) {
                 return maskCardNumbersWithHyphens(strippedCreditCard).toString();
             } else {
                 return maskCardForNumbers(strippedCreditCard).toString();
@@ -33,19 +38,19 @@ public class CardMaskingService {
     }
 
     private StringBuilder maskCardForNumbers(String cardNumber) {
-        String subSectionOfCreditCard = cardNumber.substring(6, cardNumber.length() - 4);
+        String subSectionOfCreditCard = cardNumber.substring(NUMBER_SIX, cardNumber.length() - NUMBER_FOUR);
         StringBuilder builder = new StringBuilder();
-        builder.append(cardNumber, 0, 6);
-        builder.append(String.join("", Collections.nCopies(subSectionOfCreditCard.length(), "*")));
-        builder.append(cardNumber.substring(cardNumber.length() - 4));
+        builder.append(cardNumber, NUMBER_ZERO, NUMBER_SIX);
+        builder.append(String.join("", Collections.nCopies(subSectionOfCreditCard.length(), STAR)));
+        builder.append(cardNumber.substring(cardNumber.length() - NUMBER_FOUR));
         return builder;
     }
 
     private StringBuilder maskCardNumbersWithHyphens(String cardNumber) {
         StringBuilder builder = maskCardForNumbers(cardNumber);
-        builder.insert(4, "-");
-        builder.insert(9, "-");
-        builder.insert(14, "-");
+        builder.insert(4, SEPARATION);
+        builder.insert(9, SEPARATION);
+        builder.insert(14, SEPARATION);
         return builder;
     }
 }
